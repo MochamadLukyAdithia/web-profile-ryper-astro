c652002176d61efb7723f27b3030315651033cf8
lukydev <mochamadlukyadithiawork@gmail.com>
2025-10-03 12:06:01 +0700

    feat: update blog categories in about.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://web-profile-ryper-astro.vercel.app",
+  site: "https://ryperlabfasilkomunej.vercel.app",
   output: "server",   
   adapter: vercel(),  
   vite: {

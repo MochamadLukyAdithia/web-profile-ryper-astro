020db37ba96414e514bd63649c691769b9a3bf57
lukydev <mochamadlukyadithiawork@gmail.com>
2025-11-18 22:19:48 +0700

    feat: fix icon product

--- src/components/layouts/header/navigation/MobileNav.tsx
+++ src/components/layouts/header/navigation/MobileNav.tsx
@@ -1,5 +1,5 @@
 import { NavItem } from "./NavItem";
-import { Globe, House, Users, BookOpen, Package } from "lucide-react";
+import { Globe, House, Users, BookOpen, Code2 } from "lucide-react";
 
 export const MobileNav = () => {
   return (
@@ -8,7 +8,7 @@ export const MobileNav = () => {
       <NavItem Icon={Globe} nameRoute="Blog" destinationRoute="/blogs" />
       <NavItem Icon={Users} nameRoute="About" destinationRoute="/about" />
       <NavItem Icon={BookOpen} nameRoute="Material" destinationRoute="/material" />
-      <NavItem Icon={Package} nameRoute="Produk" destinationRoute="/product" />
+      <NavItem Icon={Code2} nameRoute="Produk" destinationRoute="/product" />
     </nav>
   );
 };
